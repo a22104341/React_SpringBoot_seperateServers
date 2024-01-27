227eb697227bd268b5a648efbd55d8a60c8599e8
a22104341 <diogo.m.rodrigues2003@googlemail.com>
2024-01-27 16:06:15 +0000

    Springboot: Added some stuff to fix the CorsConfig, so i can call stuff from React-> Springboot, still need to import some things though, as idk which 2 import

--- React/tfc_react/src/App.js
+++ React/tfc_react/src/App.js
@@ -1,7 +1,26 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import {getCtos} from './springboot/CTOservice';
 
 function App() {
+  const[data, setData] = useState({});
+  const[currentPage, setCurrentPage] = useState(0);
+
+  const getCtos = async(page = 0) => {
+    try{
+      setCurrentPage(page);
+      const { data} = await getCtos();
+      setData(data);
+      console.log(data);
+    }catch(error){
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getCtos();
+  },[])
+
   return (
     <div>
         <h1>Hello</h1>
--- Springboot/tfc/src/main/java/com/tfc/tfc/constant/Constant.java
+++ Springboot/tfc/src/main/java/com/tfc/tfc/constant/Constant.java
@@ -3,4 +3,6 @@ package com.tfc.tfc.constant;
 public class Constant {
     //This makes it so i get the users Downloads folder and create a CTOs folder within it
     public static final String ctoPdf = System.getProperty("user.home") + "/Downloads/CTOs";
+
+    public static final String X_REQUESTED_WITH = "X-Requested-With";
 }
